510e3dc5639ea5a186ce5dd35112ce1e22eacda9
Cascadia-EmilioDiaz <emiliobdiaz@gmail.com>
2019-04-11 17:16:40 -0700

    Add Delegate Method - Part 2

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -4,7 +4,7 @@
 
 import UIKit
     // TODO: Mark the ViewController as conforming to the UITextFieldDelegate Protocol
-class ConversionViewController: UIViewController{ //, UITextFieldDelegate {
+class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
@@ -32,12 +32,17 @@ class ConversionViewController: UIViewController{ //, UITextFieldDelegate {
             return true
         }
     }
+    
     // DELEGATE METHOD : textFieldDidBeginEditing - is called when the user selects the text field
     // TODO: Add and modify the method to build expectation for the output by changing the celsiusLabel when the input field is selected
     // modify the celsiusLabel text to be a single question mark
     // modify the celsiusLabel color to be 60% red, 60% green, and 40% blue (refer to the Developer Documentation for UIColor)
-
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        celsiusLabel.text = "?"
+        celsiusLabel.textColor = UIColor(red: 0.60, green: 0.60, blue: 0.40, alpha: 1.0)
+    }
+
     // EVENT HANDLER METHOD : Called when TextField is Changed (notice the optional binding)
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         if let text = textField.text, let value = Double(text) {
