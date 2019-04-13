c68495c6f19156ee432f7196204d7c5195f54f5b
Sharief Youssef <sharief.youssef@gmail.com>
2019-04-13 15:11:25 -0700

    Completed part 1 of the in-class activity.

--- Get-Set-Struct-Properties.playground/Contents.swift
+++ Get-Set-Struct-Properties.playground/Contents.swift
@@ -5,12 +5,30 @@ struct Student{
     var lastName:String
     var studentID:String
     var _password:String? {
+        // property observer (another one is willSet)
         didSet{
             print("Your password was changed from \(oldValue) to \(_password!)")
         }
     }
+    var streetNumber:String
+    var streetName:String
+    var cityName:String
+    var stateABV:String
+    var zipCode:String
+    
     // Computed Properties
-    var fullName:String { return "\(firstName) \(lastName)"}
+    var fullName:String {
+        return "\(firstName) \(lastName)"
+    }
+    
+    var fullAddress:String {
+        return "\(streetNumber) \(streetName), \(cityName) \(stateABV) \(zipCode)"
+    }
+    
+    var streetAddress:String {
+        return "\(streetNumber) \(streetName)"
+    }
+    
     var password:String {
         get
         {
@@ -26,9 +44,11 @@ struct Student{
     }
 }
 
-var Ian = Student(firstName: "Ian", lastName:"Bansenauer", studentID: "920111123", _password: nil)
+var Sharief = Student(firstName: "Sharief", lastName:"Youssef", studentID: "920111123", _password: nil, streetNumber: "19128", streetName: "112th Ave NE", cityName: "Bothell", stateABV: "WA", zipCode: "98011")
 
-print("Initial password: " + Ian.password)
+print("Name: " + Sharief.fullName)
+print("Address: " + Sharief.fullAddress)
+print("Initial password: " + Sharief.password)
 //Set Password
-Ian.password = "testPassword"
-Ian.password = "Fabul0_346"
+Sharief.password = "testPassword"
+Sharief.password = "Fabul0_346"
