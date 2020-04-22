d2275a65ac8710aae2895b78cc889d0d374eff77
Clay Suttner <csuttner@gmail.com>
2020-04-22 14:19:49 -0700

    Adds studentEmail and securityQuestions computed properties to playground

--- Get-Set-Struct-Properties.playground/Contents.swift
+++ Get-Set-Struct-Properties.playground/Contents.swift
@@ -4,11 +4,13 @@ struct Student{
     var firstName:String
     var lastName:String
     var studentID:String
+
     var _password:String? {
         didSet{
-            print("Your password was changed from \(oldValue) to \(_password!)")
+            print("Your password was changed from \(oldValue ?? "nil") to \(_password!)")
         }
     }
+    
     // Computed Properties
     var fullName:String { return "\(firstName) \(lastName)"}
     var password:String {
@@ -24,11 +26,52 @@ struct Student{
             _password = newValue
         }
     }
+    
+    var studentEmail: String {
+        var email = String(firstName.prefix(1))
+        email.append(lastName)
+        email.append("@student.cascadia.edu")
+        return email
+    }
+    
+    var _securityQuestions: [String]? {
+        didSet {
+            print("Your security questions were changed from:")
+            printSecurityQuestions(oldValue)
+            print("to:")
+            printSecurityQuestions(_securityQuestions)
+        }
+    }
+    
+    var securityQuestions: [String] {
+        get {
+            if let _securityQuestions = _securityQuestions { return _securityQuestions }
+            return []
+        }
+        set {
+            _securityQuestions = newValue
+        }
+    }
+    
+    private func printSecurityQuestions(_ questions: [String]?) {
+        if let questions = questions {
+            for question in questions {
+                print(question)
+            }
+        }
+    }
+    
 }
 
-var Ian = Student(firstName: "Ian", lastName:"Bansenauer", studentID: "920111123", _password: nil)
+var Ian = Student(firstName: "Ian", lastName:"Bansenauer", studentID: "920111123", _password: nil, _securityQuestions: nil)
 
 print("Initial password: " + Ian.password)
 //Set Password
 Ian.password = "testPassword"
 Ian.password = "Fabul0_346"
+print()
+Ian.securityQuestions.append("What is my favorite color?")
+print()
+Ian.securityQuestions.append("What is my favorite animal?")
+
+
