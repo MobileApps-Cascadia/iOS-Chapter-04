f46a0a8aa087e2e45089d786d715f554ef543c95
Charles Sy <charles.m.sy@hotmail.com>
2020-04-22 13:55:20 -0700

    Try out playground. added getter and setter properties on last name.

--- Get-Set-Struct-Properties.playground/Contents.swift
+++ Get-Set-Struct-Properties.playground/Contents.swift
@@ -2,7 +2,22 @@
 struct Student{
     //Stored Properties
     var firstName:String
-    var lastName:String
+    var _lastName:String? {
+        didSet {
+            print("Your last name changed from \(oldValue) to \(_lastName!)")
+        }
+    }
+    
+    var lastName:String? {
+        get
+        {
+            return _lastName
+        }
+        set{
+            _lastName = newValue
+        }
+    }
+    
     var studentID:String
     var _password:String? {
         didSet{
@@ -10,15 +25,21 @@ struct Student{
         }
     }
     // Computed Properties
-    var fullName:String { return "\(firstName) \(lastName)"}
+    var fullName:String {
+        return "\(firstName) \(_lastName)"
+        
+    }
+    
     var password:String {
         get
         {
             // Password has been set
-            if let _password = _password { return _password }
+            if let _password = _password {
+                return _password
+            }
             // Provide initial, temporary password
             let last4 = studentID.suffix(4)
-            return "\(lastName)\(last4)"
+            return "\(_lastName)\(last4)"
         }
         set{
             _password = newValue
@@ -26,9 +47,14 @@ struct Student{
     }
 }
 
-var Ian = Student(firstName: "Ian", lastName:"Bansenauer", studentID: "920111123", _password: nil)
+
+
+
+var Ian = Student(firstName: "Ian", _lastName:"Bansenauer", studentID: "920111123", _password: nil)
 
 print("Initial password: " + Ian.password)
 //Set Password
 Ian.password = "testPassword"
 Ian.password = "Fabul0_346"
+Ian.password = "Covid-19"
+Ian.lastName = "Smith"
